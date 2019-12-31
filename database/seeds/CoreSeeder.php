950cd1df4a93eef6aeb4c0995fbb61fbe83cede2
MOC Soluções <maike@mocsolucoes.com.br>
2019-12-31 16:33:19 -0300

    Database Seeder to CoreSeeder

--- database/seeds/CoreSeeder.php
+++ database/seeds/CoreSeeder.php
@@ -1,12 +1,8 @@
 <?php
 
-use MOCSolutions\Auth\Models\Perfil;
-use MOCSolutions\Auth\Models\Permissao;
-use MOCSolutions\Auth\Models\Usuario;
-use MOCSolutions\Core\Models\Configuracao;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CoreSeeder extends Seeder
 {
     /**
      * Run the database seeds.
