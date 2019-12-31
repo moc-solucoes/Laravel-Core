2e8c687701c45b6ec336b7011844d47821a26531
MOC Soluções <maike@mocsolucoes.com.br>
2019-12-31 14:07:57 -0300

    Create a empty Database seeder

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use MOCSolutions\Auth\Models\Perfil;
+use MOCSolutions\Auth\Models\Permissao;
+use MOCSolutions\Auth\Models\Usuario;
+use MOCSolutions\Core\Models\Configuracao;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Implements the seed run here.
+    }
+}
