a02edc5441b181067fa95daec0f20e6cc3107911
Maike Oliveira <maike@mocsolucoes.com.br>
2020-08-20 23:37:29 -0300

    Ajuste na licença.

--- Examples/shared/_able-menu.blade.php
+++ Examples/shared/_able-menu.blade.php
@@ -60,47 +60,7 @@
                 </li>
             </ul>
         </li>
-        @if(
-           hasPermission('administrar.usuarios') ||
-           hasPermission('administrar.usuarios.perfis') ||
-           hasPermission('administrar.usuarios.permissoes') ||
-           hasPermission('administrar.usuarios.secretarias')
-        )
-            <div class="pcoded-navigation-label">Usuários</div>
-            <li class="" {{!hasPermission('administrar.usuarios.secretarias') ? 'hidden' : ''}}>
-                <a href="#" class="waves-effect waves-dark">
-       <span class="pcoded-micon">
-           <i class="feather icon-server"></i>
-       </span>
-                    <span class="pcoded-mtext">Secretarias</span>
-                </a>
-            </li>
-            <li class="" {{!hasPermission('administrar.usuarios') ? 'hidden' : ''}}>
-                <a href="{{route('usuario.listar')}}" class="waves-effect waves-dark">
-       <span class="pcoded-micon">
-           <i class="feather icon-user"></i>
-       </span>
-                    <span class="pcoded-mtext">Usuários</span>
-                </a>
-            </li>
-            <li class="" {{!hasPermission('administrar.usuarios.perfis') ? 'hidden' : ''}}>
-                <a href="{{route('usuario.perfil.listar')}}" class="waves-effect waves-dark">
-       <span class="pcoded-micon">
-           <i class="fa fa-product-hunt"></i>
-       </span>
-                    <span class="pcoded-mtext">Perfis</span>
-                </a>
-            </li>
-            <li class="" {{!hasPermission('administrar.usuarios.permissoes') ? 'hidden' : ''}}>
-                <a href="{{route('usuario.permissao.listar')}}" class="waves-effect waves-dark">
-       <span class="pcoded-micon">
-           <i class="fa fa-user-secret"></i>
-       </span>
-                    <span class="pcoded-mtext">Permissões</span>
-                </a>
-            </li>
-        @endif
-
+        @include('Auth::_user_menu_admin')
         <li class="">
             <a href="{{route('usuario.logout')}}" class="waves-effect waves-dark">
        <span class="pcoded-micon">
